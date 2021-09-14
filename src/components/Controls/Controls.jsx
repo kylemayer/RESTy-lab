3d2ccf8176d49c03b5272347a9df07ee60793b18
Kyle Mayer <x@kylemayer.dev>
2021-09-14 08:45:36 -0700

    file structure

---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Controls = ({}) => (
+  <form>
+
+
+  </form>
+);
+
+
+Controls.propTypes = {
+
+};
+
+export default Controls;

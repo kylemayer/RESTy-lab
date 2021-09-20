d6995737cb9d317119d8c6948b1d719b2a64bfeb
Kyle Mayer <x@kylemayer.dev>
2021-09-20 08:20:10 -0700

    fetch req and container

--- src/containers/Container.jsx
+++ src/containers/Container.jsx
@@ -5,21 +5,31 @@ import Controls from '../components/controls/Controls';
 
 export default class Container extends Component {
   state = {
-
+    url: '',
+    method: '',
+    body: '',
+    response: [],
   };
 
-
-
-
-
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   render() {
+    const { url, method, body, response } = this.state;
+
     return (
       <>
         <h1> Testing Testing</h1>
+        <Controls
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit}
+          url={url}
+          method={method}
+          body={body}
+        />
+        <Body response={response} />
         <Sidebar />
-        <Controls />
-        <Body />
 
       </>
     );
--- src/services/fetchReq.js
+++ src/services/fetchReq.js
@@ -1,2 +1,10 @@
-export const fetchReq = ({
-});
+const fetchReq = (url, method = 'GET', body) => {
+  return fetch(url, {
+    method,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  })
+    .then(res => res.json());
+};
+
+export default fetchReq;

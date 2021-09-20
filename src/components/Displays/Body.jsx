a43f51981066a9f77d98010dc7ad55ae9117e49d
Kyle Mayer <x@kylemayer.dev>
2021-09-20 08:43:07 -0700

    body component

--- src/components/Displays/Body.jsx
+++ src/components/Displays/Body.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactJson from 'react-json-view';
 
-const Body = () => <div></div>;
-
-Body.propTypes = {};
+const Body = ({ response }) => {
+  return (
+    <section aria-label="response">
+      <ReactJson src={response} />
+    </section>
+  );
+};
+Body.propTypes = {
+  response: PropTypes.any,
+};
 
 export default Body;
